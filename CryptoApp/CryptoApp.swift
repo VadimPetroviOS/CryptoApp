8d143e879831cd81121626b7b2a4fe38814ea895
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-03-18 16:37:42 +0300

    Setting up Xcode Project and a custom color theme

---
+++ CryptoApp/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Вадим on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
